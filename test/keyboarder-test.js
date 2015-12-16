91eb7ebb818badc46209caf9f9e352ecb6cf65f8
Tyler Graham <tylergraham134@gmail.com>
2015-12-15 17:06:14 -0700

    small changes to keyboarder test file and other formatting

--- test/keyboarder-test.js
+++ test/keyboarder-test.js
@@ -5,15 +5,18 @@ const Board = require('../lib/board');
 
 describe('Keyboarder', function() {
   beforeEach(function() {
+    this.keyboarder = new keyboarder();
     this.board = new Board();
     this.player = new Player(this.board);
     this.originalCenterX = this.player.center.x;
   });
 
-  xit('should know if the left arrow key is pressed down', function() {
-    let player = new Player(this.board);
-    let originalCenterX = player.center.x;
+  xit('should move the player left if the left arrow key is pressed down', function() {
+    // var e = $.Event("keydown");
+    // e.keyCode = 50;
     let result = player.keyboarder.isDown();
+
+
 
     player.update();
 
